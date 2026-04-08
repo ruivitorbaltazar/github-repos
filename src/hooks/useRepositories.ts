73517a32fd880f713ccf8c5c183d0ef977fbad87
Rui Baltazar <ruivrb@gmail.com>
2026-04-08 16:59:17 +0100

    feat: added dynamic search by keyword, language

--- src/hooks/useRepositories.ts
+++ src/hooks/useRepositories.ts
@@ -1,19 +1,31 @@
+import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query"
 import { fetchRepositories } from "../api/github"
 import { Repository } from "../types/repository"
 
-export const useRepositories = () => {
+export const useRepositories = ({ search, language }: { search: string; language: string }) => {
+  const [debouncedSearch, setDebouncedSearch] = useState(search);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedSearch(search.toLowerCase());
+    }, 400);
+
+    return () => clearTimeout(timeout);
+  }, [search]);
+
   const refetchInterval = 60000 // 1 minute
 
-  const { data, isLoading, error } = useQuery<Repository[]>({
-    queryKey: ["repositories"],
+  const { data, isFetching, error } = useQuery<Repository[]>({
+    queryKey: ["repositories", { search: debouncedSearch, language }],
     queryFn: fetchRepositories,
     refetchInterval: refetchInterval,
+    placeholderData: (previousData) => previousData,
   })
 
   return {
     reposData: data,
-    reposAreLoading: isLoading,
+    reposAreFetching: isFetching,
     reposError: error,
   }
 }
